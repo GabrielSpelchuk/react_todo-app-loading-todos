719eba730d6326cdc1af5e5937652247425b6411
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-03-13 20:38:40 +0200

    add solution

---
+++ src/components/TodoFooter.tsx
@@ -0,0 +1,66 @@
+import classNames from 'classnames';
+
+interface ITodoFooter {
+  todoCount: number | undefined;
+  filter: string;
+  setFilter: (filter: string) => void;
+}
+
+export const TodoFooter: React.FC<ITodoFooter> = ({
+  todoCount,
+  filter,
+  setFilter,
+}) => {
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {todoCount} items left
+      </span>
+
+      {/* Active link should have the 'selected' class */}
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={classNames('filter__link', {
+            selected: filter === 'all',
+          })}
+          data-cy="FilterLinkAll"
+          onClick={() => setFilter('all')}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames('filter__link', {
+            selected: filter === 'active',
+          })}
+          data-cy="FilterLinkActive"
+          onClick={() => setFilter('active')}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames('filter__link', {
+            selected: filter === 'completed',
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => setFilter('completed')}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {/* this button should be disabled if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
